7139b880dee4aeb880e52ff521fcc09591536d28
penz6 <thetechkid66@gmail.com>
2021-05-12 10:56:26 -0400

    Added Embeds and slash commands

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from chatterbot import ChatBot
 from discord.ext import commands
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import logging
+from discord_slash import SlashCommand, SlashContext
 from token_2 import token_2
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.default())
@@ -24,6 +25,11 @@ async def on_message(message):
             user_input = message.content
             bot_response = chatbot.get_response(user_input)
             await message.channel.send(bot_response)
+
+@slash.slash(name="test")
+async def _test(ctx: SlashContext):
+    embed = discord.Embed(title="embed test")
+    await ctx.send(content="test", embeds=[embed])
 
 @bot.event
 async def on_connect():
