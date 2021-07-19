1d8b7589fc29595337b170f79672605058dffd63
penz6 <thetechkid66@gmail.com>
2021-07-19 00:50:39 -0400

    multiple choice polls

--- bot.py
+++ bot.py
@@ -14,10 +14,22 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author.id == 586273007077687316 and "poll" in message.content.lower():
-        check1 = '<:check:862204980144373790'
+        check1 = '<:check:862204980144373790>'
         nope = '<:nope:862205092161519616>'
         await message.add_reaction(check1)
         await message.add_reaction(nope)
+
+#3 option / abc
+@bot.event
+async def on_message(message):
+    if message.author.id == 586273007077687316 and "abc" in message.content.lower():
+        emojia = '🇦'
+        emojib = '🇧'
+        emojic = '🇨'
+        await message.add_reaction(emojia)
+        await message.add_reaction(emojib)
+        await message.add_reaction(emojic)
+
 
 @bot.event
 async def on_connect():
