3474f2d9c11dc1ec50baecae989d8a7d85054302
penz6 <thetechkid66@gmail.com>
2021-07-19 01:08:11 -0400

    added a bigger poll and a help command

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import discord
 from token_2 import token_2
 from discord.ext import commands
+from discord_slash import SlashCommand
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.default())
-check = "<:check:862204980144373790>"
-nope = "<:nope:862205092161519616>"
+slash = SlashCommand(bot, sync_commands=True)
+
+#startup message
 @bot.event
 async def on_ready():
     print("Things are maybe working")
@@ -30,7 +32,31 @@ async def on_message(message):
         await message.add_reaction(emojib)
         await message.add_reaction(emojic)
 
+#4 option / abcd
+@bot.event
+async def on_message(message):
+    if message.author.id == 586273007077687316 and "abcd" in message.content.lower():
+        emojia = '🇦'
+        emojib = '🇧'
+        emojic = '🇨'
+        emojid = '🇩'
+        await message.add_reaction(emojia)
+        await message.add_reaction(emojib)
+        await message.add_reaction(emojic)
+        await message.add_reaction(emojid)
 
+
+#help command
+@slash.slash(name="poll",description="This is only really useful to admins")
+async def poll(ctx):
+    embedpollhelp = discord.Embed(title="Poll Commands", description="Only really useful to admins", color=0x00ff00)
+    embedpollhelp.add_field(name="Check and X", value="Have the word poll in you message", inline=False)
+    embedpollhelp.add_field(name="3 choices or ABC", value="Have abc in your message", inline=False)
+    embedpollhelp.add_field(name="4 choices or ABCD", value="Have abcd in your message", inline=False)
+    await ctx.send(embed=embedpollhelp)
+
+
+#status
 @bot.event
 async def on_connect():
     await bot.change_presence(
