02915979b74ad1061b56a684a74943c5e2bead6a
penz6 <71047716+penz6@users.noreply.github.com>
2022-03-20 20:59:32 -0400

    replace file

--- aibot.py
+++ aibot.py
@@ -2,24 +2,29 @@ from chatterbot import ChatBot
 import interactions
 from token_2 import token_2
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from interactions import Client, Intents
 #imports
-bot = interactions.Client(token=token_2)
+bot = interactions.Client(token=token_2,intents=Intents.DEFAULT | Intents.GUILD_MESSAGE_CONTENT)
 #bot token
 #starts bot
 chatbot = ChatBot("Fream")
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train(
-    "chatterbot.corpus.english"
-)
+#trainer = ChatterBotCorpusTrainer(chatbot)
+#trainer.train(
+ #   "chatterbot.corpus.english"
+#)
 #trains chatbot
-
-@client.event
+@bot.event
 async def on_message_create(message):
     if "fream" in message.content.lower():
-         user_input = message.content
-         bot_response = chatbot.get_response(user_input)
-         await ctx.respond(bot_response)
+        if message.author.bot: return;
+        user_input = message.content
+       #message content to feed to chatbot
+        bot_response = chatbot.get_response(user_input)
+       #feeds message content to chatbot
+        message._client = bot._http
+       #http client error hotfix
+        bot_response2 = str(bot_response)
+        await message.reply(bot_response2)
         
 print("started")
 bot.start()
-
